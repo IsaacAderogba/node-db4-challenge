54c972c5096581add2c8978b724db3f2c7cccd65
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-18 13:56:01 +0100

    Add middleware for recipes resource

--- services/recipes/routes.js
+++ services/recipes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./controllers");
+const { validateRecipeId } = require("./middleware");
 
 router.get("/recipes", async (req, res, next) => {
   try {
@@ -11,32 +12,54 @@ router.get("/recipes", async (req, res, next) => {
   }
 });
 
-router.get("/recipes/:id/shoppingList", async (req, res, next) => {
+router.get(
+  "/recipes/:id/shoppingList",
+  validateRecipeId,
+  async (req, res, next) => {
+    try {
+      const shoppingList = await controller.getRecipeIngredients(req.params.id);
+
+      const recipeWithList = {
+        ...req.recipe,
+        shoppingList
+      };
+
+      res.status(200).json(recipeWithList);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get(
+  "/recipes/:id/instructions",
+  validateRecipeId,
+  async (req, res, next) => {
+    try {
+      const instructions = await controller.getRecipeInstructions(
+        req.params.id
+      );
+
+      const recipeWithInstructions = {
+        ...req.recipe,
+        instructions
+      };
+
+      res.status(200).json(recipeWithInstructions);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get("/recipes/:id", validateRecipeId, async (req, res, next) => {
   try {
-    const shoppingList = await controller.getRecipeIngredients(req.params.id);
-    res.status(200).json(shoppingList);
+    const recipe = await controller.getRecipeById(req.params.id);
+
+    res.status(200).json(recipe);
   } catch (err) {
     next(err);
   }
 });
-
-router.get("/recipes/:id/instructions", async (req, res, next) => {
-  try {
-    const instructions = await controller.getRecipeInstructions(req.params.id);
-    res.status(200).json(instructions);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get("/recipes/:id", async (req, res, next) => {
-  try {
-    const recipe = await controller.getRecipeById(req.params.id)
-
-    res.status(200).json(recipe)
-  } catch (err) {
-    next(err);
-  }
-})
 
 module.exports = router;
